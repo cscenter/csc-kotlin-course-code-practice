cf0df09bb762a8a64973909da7ab2e69394f3a0d
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-06-28 17:08:36 +0200

    Add atomicfu

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,27 @@
+
 plugins {
     kotlin("jvm") version "1.8.21"
     application
 }
+
+// atomicfu
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.21.0")
+    }
+}
+apply(plugin = "kotlinx-atomicfu")
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://plugins.gradle.org/m2/")
+    }
 }
+
 
 dependencies {
     testImplementation(kotlin("test"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
 
 rootProject.name = "csc-kotlin-course-code-practice"
 
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
