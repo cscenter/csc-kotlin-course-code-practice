d6580be97d7329375eff7465cd62693742761931
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-06-28 17:32:55 +0200

    Fix tests and function definition

--- src/main/kotlin/cub/concurrent/answers/Task1.kt
+++ src/main/kotlin/cub/concurrent/answers/Task1.kt
@@ -7,14 +7,14 @@ import kotlin.random.Random
 class Task1 {
     private val sum = atomic(0)
 
-    fun parallelSum(list: List<Int>, nThreads: Int): Int {
+    fun List<Int>.parallelSum(nThreads: Int): Int {
         require(nThreads > 0)
-        if (list.isEmpty())
+        if (isEmpty())
             return 0
         // It is better to handle this case separately to avoid creating new threads, etc
         if (nThreads == 1)
-            return list.sum()
-        list.chunked(nThreads)
+            return sum()
+        chunked(nThreads)
             .map {
                 thread {
                     sum += it.sum()
@@ -29,13 +29,15 @@ class Task1 {
 
 fun main() {
     val task1 = Task1()
-    check(task1.parallelSum(listOf(), nThreads = 1) == 0)
+    with(task1) {
+        check(listOf<Int>().parallelSum(nThreads = 1) == 0)
 
-    check(task1.parallelSum(listOf(1, 2, 3), nThreads = 1) == 6)
+        check(listOf(1, 2, 3).parallelSum(nThreads = 1) == 6)
 
-    val list = (0 .. 4000).toList()
-    check(task1.parallelSum(list, nThreads = 4) == list.sum())
+        val list = (0 .. 4000).toList()
+        check(list.parallelSum(nThreads = 4) == list.sum())
 
-    val randomList = sequence { yield(Random.nextInt() % 100) }.take(4000).toList()
-    check(task1.parallelSum(randomList, nThreads = 4) == randomList.sum())
+        val randomList = sequence { yield(Random.nextInt() % 100) }.take(4000).toList()
+        check(randomList.parallelSum(nThreads = 4) == randomList.sum())
+    }
 }
--- src/main/kotlin/cub/concurrent/tasks/Task1.kt
+++ src/main/kotlin/cub/concurrent/tasks/Task1.kt
@@ -1,8 +1,13 @@
 package cub.concurrent.tasks
 
+import cub.concurrent.answers.Task1
+import kotlinx.atomicfu.atomic
 import kotlin.random.Random
 
-/* Implement parallel sum for the list.
+class Task1 {
+    private val sum = atomic(0)
+
+    /* Implement parallel sum for the list.
  * The function should split the list into chunks of equal size,
  * calculate the sum of each chunk into separate thread,
  * and then merge the results to get the final sum.
@@ -20,19 +25,23 @@ import kotlin.random.Random
  * NN
  * (single number -- sum of the list)
  */
-fun parallelSum(list: List<Int>, nThreads: Int): Int {
-    require(nThreads > 0)
-    TODO("Implement your solution here")
+    fun List<Int>.parallelSum(nThreads: Int): Int {
+        require(nThreads > 0)
+        TODO("Implement your solution here")
+    }
 }
 
 fun main() {
-    check(parallelSum(listOf(), nThreads = 1) == 0)
+    val task1 = Task1()
+    with(task1) {
+        check(listOf<Int>().parallelSum(nThreads = 1) == 0)
 
-    check(parallelSum(listOf(1, 2, 3), nThreads = 1) == 6)
+        check(listOf(1, 2, 3).parallelSum(nThreads = 1) == 6)
 
-    val list = (0 .. 4000).toList()
-    check(parallelSum(list, nThreads = 4) == list.sum())
+        val list = (0 .. 4000).toList()
+        check(list.parallelSum(nThreads = 4) == list.sum())
 
-    val randomList = sequence { yield(Random.nextInt() % 100) }.take(4000).toList()
-    check(parallelSum(randomList, nThreads = 4) == randomList.sum())
+        val randomList = sequence { yield(Random.nextInt() % 100) }.take(4000).toList()
+        check(randomList.parallelSum(nThreads = 4) == randomList.sum())
+    }
 }
